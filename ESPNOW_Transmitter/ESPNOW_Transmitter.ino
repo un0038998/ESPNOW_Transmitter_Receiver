826c664f7561081061c6eb61afced67d48a163c1
FineQuasar17 <86128535+FineQuasar17@users.noreply.github.com>
2024-01-02 17:47:04 +0000

    fix error in incompleate setup of espnow

--- ESPNOW_Transmitter/ESPNOW_Transmitter.ino
+++ ESPNOW_Transmitter/ESPNOW_Transmitter.ino
@@ -76,7 +76,9 @@ void setup()
   esp_now_register_send_cb(OnDataSent);
   
   // Register peer
-  esp_now_peer_info_t peerInfo;
+//esp_now_peer_info_t peerInfo;
+  esp_now_peer_info_t peerInfo = {};
+
   memcpy(peerInfo.peer_addr, receiverMacAddress, 6);
   peerInfo.channel = 0;  
   peerInfo.encrypt = false;
